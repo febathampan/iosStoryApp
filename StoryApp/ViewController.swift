66cbe353afbc2085a8631da06e6f2eb54234bfbd
user234888 <user234888@NY551.local>
2023-10-13 10:06:28 -0400

    story initial commit

---
+++ StoryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryApp
+//
+//  Created by user234888 on 10/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
